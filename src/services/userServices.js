98c731e9a25113f1e44cb1d72aa143d00b50e249
= <=>
2020-05-09 02:43:22 +0530

    [vishnu]Refactor.added new function for login

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,14 +1,26 @@
 import axios from "axios";
-import userApiConstants from "../constants/userApiConstants"
+import userApiConstants from "../constants/userApiConstants";
 
 async function userRegistration(registerData) {
-    try {
-        const response = await axios.post(process.env.REACT_APP_BASE_URL + userApiConstants.registration, registerData);
-        return response;
-    } catch (err) {
-        throw err;
-    }
+  try {
+    const response = await axios.post(
+      process.env.REACT_APP_BASE_URL + userApiConstants.registration,
+      registerData
+    );
+    return response;
+  } catch (err) {
+    throw err;
+  }
 }
 
+export async function login(loginData) {
+  try {
+    const response = await axios.post(
+      process.env.REACT_APP_BASE_URL + userApiConstants.login,
+      loginData
+    );
+    return response;
+  } catch (error) {}
+}
 
-export default userRegistration 
+export default userRegistration;
