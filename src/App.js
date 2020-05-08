45ecda37ae013aabbe56becf19a00e7062269339
= <=>
2020-05-09 02:40:26 +0530

    [Vishnu]Refactor.added routes to login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Login from "./component/login";
 import Registration from "./component/registration";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -9,6 +10,7 @@ class App extends React.Component {
       <Router>
         <Switch>
           <Route path="/registration" component={Registration}></Route>
+          <Route path="/login" component={Login}></Route>
         </Switch>
       </Router>
     );
