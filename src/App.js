fbb02aeb9ba6a1ca27589358832a03b9d097eaf6
= <=>
2020-05-13 19:47:05 +0530

    [Vishnu]Added route to dashboard

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from "./component/login";
 import Registration from "./component/registration";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ForgetPassword from "./component/forgetPassword";
+import Dashboard from "./component/dashboard"
 
 class App extends React.Component {
   render() {
@@ -13,6 +14,7 @@ class App extends React.Component {
           <Route path="/registration" component={Registration}></Route>
           <Route path="/login" component={Login}></Route>
           <Route path="/forgetPassword" component={ForgetPassword}></Route>
+          <Route path='/dashboard' component={Dashboard}/>
         </Switch>
       </Router>
     );
