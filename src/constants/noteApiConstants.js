ff43d7587e3451e2839e0ca82069ca9b461e5fe6
= <=>
2020-05-17 00:10:23 +0530

    [Vishnu]Add.added note api constants

---
+++ src/constants/noteApiConstants.js
@@ -0,0 +1,10 @@
+module.exports = {
+  addNotes: "notes/addNotes",
+  getNotes: "/notes/getNotesList",
+  updateNotes: "/notes/updateNotes",
+  changeColor: "/notes/changesColorNotes",
+  pinNotes: "/notes/pinUnpinNotes",
+  labelNote: "/noteLabels",
+  trashNote: "/notes/trashNotes",
+  archive: "/notes/archiveNotes",
+};
