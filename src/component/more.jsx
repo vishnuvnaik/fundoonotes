bc314208a2780350f92160cd5431fb0999089f71
= <=>
2020-05-22 02:36:12 +0530

    [Vishnu]Add.added more menu(need to modify)

---
+++ src/component/more.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MoreMenu extends Component {
+    render() {
+        return (
+            <div>
+              something  
+            </div>
+        )
+    }
+}
