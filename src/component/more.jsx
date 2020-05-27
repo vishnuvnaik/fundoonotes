6cb560457dff1b5103139aed9ef16bc01eb113c6
= <=>
2020-05-28 00:04:14 +0530

    [Vishnu]Refactor.addlabelmenu component called

--- src/component/more.jsx
+++ src/component/more.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
-import { Popover, Dialog, Menu } from "@material-ui/core";
+import { Popover, Dialog, Menu, MenuItem } from "@material-ui/core";
 import noteService from "../services/noteServices";
 import LabelMenu from "./labelMenu";
 import AddLabelNote from "./addLabel";
 import "./CSS/dashboard.css";
 class MoreMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: this.props.moreMenuOpen,
+      menuAnchor: this.props.moreMenuAnchor,
+      labelMenu: this.props.labelOpen,
+      labelAnchorEL: null,
+    };
+  }
   deleteNote = () => {
     const field = {
       isDeleted: true,
@@ -15,14 +24,7 @@ class MoreMenu extends Component {
       this.props.getNote();
     });
   };
-  handleClickLabel = (event) => {
-    this.setState({
-      menuOpen: false,
-      menuanchorEl: false,
-      labelMenu: true,
-      labelAnchorEl: event.currentTarget,
-    });
-  };
+
   handlelabel = (event) => {
     const field2 = {
       label: event.currentTarget,
@@ -31,6 +33,14 @@ class MoreMenu extends Component {
   };
   labelIdListChange = () => {
     this.setState({ labelIdList: this.state.labelIdList });
+  };
+  handleClickLabel = (event) => {
+    this.setState({
+      menuOpen: false,
+      menuAnchor: false,
+      labelMenu: true,
+      labelAnchorEL: event.currentTarget,
+    });
   };
 
   render() {
@@ -54,13 +64,12 @@ class MoreMenu extends Component {
           onClick={this.props.menu}
           className="moreMenu_content"
         >
-          <AddLabelNote
-            labelIdList={this.state.labelIdList}
-            labelIdListChange={this.labelIdListChange}
-          />
+          <MenuItem>
+            <AddLabelNote />
+          </MenuItem>
         </div>
-        <div className="moreMenu_content" onClick={this.deleteNote}>
-          <span>Delete note</span>
+        <div className="moreMenu_content">
+          <MenuItem onClick={this.deleteNote}> Delete</MenuItem>
         </div>
       </Popover>
     );
