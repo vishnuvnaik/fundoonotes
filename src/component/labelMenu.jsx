e61ee439cbd36772e5c5dbd8662d4d1041be5181
= <=>
2020-05-27 23:39:39 +0530

    [Vishnu]Refactor.added labelmenu component to display and add label from sidebar

--- src/component/labelMenu.jsx
+++ src/component/labelMenu.jsx
@@ -1,67 +1,142 @@
 import React, { Component } from "react";
-import search from "../assets/search.svg";
-import tickon from "../assets/tickon.svg";
-import tickoff from "../assets/tickoff.svg";
-import plus from "../assets/plus.svg";
-import { Popover } from "@material-ui/core";
+import { Button } from "@material-ui/core";
+import LabelIcon from "@material-ui/icons/Label";
+import AddIcon from "@material-ui/icons/Add";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import noteServices from "../services/noteServices";
+import Dialog from "@material-ui/core/Dialog";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import InputBase from "@material-ui/core/InputBase";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import LabelOutlinedIcon from "@material-ui/icons/LabelOutlined";
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from "@material-ui/icons/Edit";
 import "./CSS/dashboard.css";
 class LabelMenu extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      labelName: "",
+      open: false,
+      labels: [],
+      enterLable: "",
     };
+    this.getLables();
   }
-  handleClick = () => {
-    this.props.changeLabel(this.state.labelName);
+
+  handleClickOpen = () => {
+    this.setState({ open: !this.state.open });
+  };
+  onChangeLable = (e, index) => {
+    this.state.labels[index].label = e.target.value;
+    this.setState({ labels: this.state.labels });
+  };
+  editNotelabel = (labelId, index) => {
+    let editdata = { label: this.state.labels[index].label };
+    noteServices.updateNoteLabel(labelId, editdata);
+    this.getLables();
+  };
+  addLabel = () => {
+    let userId = JSON.parse(localStorage.getItem("userDetails")).userId;
+    noteServices.addNoteLabel(this.state.enterLable);
+    this.setState({ enterLable: "" });
+    this.getLables();
+    this.getLables();
+  };
+  getLables = async () => {
+    await noteServices
+      .getNoteLabel()
+      .then((response) =>
+        this.setState({ labels: response.data.data.details })
+      );
+  };
+  deleteLabel = (labelId) => {
+    noteServices.deleteNotelable(labelId);
+    this.getLables();
   };
   render() {
     return (
-      <Popover
-        open={true}
-        onClose={this.props.labelClose}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "center",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "center",
-        }}
-        anchorEl={this.props.anchor}
-      >
-        <div className="label_conTop">
-          <div>
-            <span>Label note</span>
-          </div>
-          <div className="label_search">
-            <input
-              type="text"
-              autoFocus
-              value={this.state.labelName}
-              onChange={(event) => {
-                this.setState({
-                  labelName: event.target.value,
-                });
-              }}
-              placeholder="Enter label name"
-            />
-            <img src={search} alt="l" />
-          </div>
+      <div className="labelContainer">
+        <div className="labelHeader">
+          <div>Labels</div>
+          <Button
+            variant="outlined"
+            size="small"
+            color="primary"
+            onClick={this.handleClickOpen}
+          >
+            Edit
+          </Button>
         </div>
-        <div className="label_conBot">
-          <div className="label_contents">
-            <img src={tickoff} alt="" />
-            <span>hello</span>
-          </div>
+        <div className="labelList">
+          {this.state.labels.map((ele) => {
+            return (
+              <div className="label">
+                <LabelIcon />
+                {ele.label}
+              </div>
+            );
+          })}
         </div>
-        {this.state.labelName !== "" ? (
-          <div onClick={this.handleClick} className="labelAdd_pos">
-            <img src={plus} alt="p" />
-            <span>{this.state.labelName}</span>
-          </div>
-        ) : null}
-      </Popover>
+        <Dialog
+          id="simple-dialog-label"
+          onClose={this.handleClickOpen}
+          aria-labelledby="simple-dialog-title"
+          open={this.state.open}
+        >
+          <DialogTitle id="dialogBoxtitleLabel">Edit Labels</DialogTitle>
+          <List>
+            <div className="addLabelHead">
+              <AddIcon />
+              <InputBase
+                placeholder="enter label..."
+                value={this.state.enterLable}
+                onChange={(e) => {
+                  this.setState({ enterLable: e.target.value });
+                }}
+                inputProps={{ "aria-label": "naked" }}
+              />
+              <IconButton size="small" onClick={this.addLabel}>
+                <CheckCircleIcon fontSize="small" />
+              </IconButton>
+            </div>
+            {this.state.labels.map((ele, index) => {
+              return (
+                <div className="editLabellist">
+                  <LabelIcon />
+                  <InputBase
+                    value={ele.label}
+                    onChange={(e) => this.onChangeLable(e, index)}
+                    inputProps={{ "aria-label": "naked" }}
+                  />
+                  <IconButton
+                    onClick={(e) => this.editNotelabel(ele.id, index)}
+                    size="small"
+                  >
+                    <EditIcon fontSize="small" />
+                  </IconButton>
+                  <IconButton
+                    size="small"
+                    onClick={(e) => this.deleteLabel(ele.id)}
+                  >
+                    <DeleteOutlineIcon fontSize="small" />
+                  </IconButton>
+                </div>
+              );
+            })}
+            <div className="editLableFooter">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={this.handleClickOpen}
+              >
+                Done
+              </Button>
+            </div>
+          </List>
+        </Dialog>
+      </div>
     );
   }
 }
