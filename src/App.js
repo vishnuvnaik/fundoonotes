7e3a00453836c713f99266c1aab0ac5b3a7afad8
= <=>
2020-06-03 02:22:28 +0530

    [Vishnu]Refactor.added routes to other note components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from "./component/login";
 import Registration from "./component/registration";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ForgetPassword from "./component/forgetPassword";
-import Dashboard from "./component/dashboard"
+import Dashboard from "./component/dashboard";
 
 class App extends React.Component {
   render() {
@@ -14,7 +14,7 @@ class App extends React.Component {
           <Route path="/registration" component={Registration}></Route>
           <Route path="/login" component={Login}></Route>
           <Route path="/forgetPassword" component={ForgetPassword}></Route>
-          <Route path='/dashboard' component={Dashboard}/>
+          <Route path="/dashboard" component={Dashboard} />
         </Switch>
       </Router>
     );
