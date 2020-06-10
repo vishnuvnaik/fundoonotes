6c2a2ca0e980a6a6c6f8ec7c87618254346b388a
= <=>
2020-06-11 01:03:11 +0530

    [Vishnu]Refactor.modified qanda like part

--- src/component/qAndA.jsx
+++ src/component/qAndA.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
-import { Divider, Typography, IconButton } from "@material-ui/core";
+import { Divider, Typography, IconButton, Tooltip } from "@material-ui/core";
 import { Editor } from "react-draft-wysiwyg";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import ThumbDownAltIcon from "@material-ui/icons/ThumbDownAlt";
@@ -149,6 +149,7 @@ export default class QueAndAns extends Component {
                 Close
               </Button>
             </div>
+            <div style={{ fontWeight: "bold" }}>Ask a Question</div>
             <Editor
               editorState={this.state.editorState}
               placeholder="enter question"
@@ -168,129 +169,139 @@ export default class QueAndAns extends Component {
             </div>
           </div>
         </div>
-        <Divider />
-        <div className={this.state.question ? "showNew margin-20px" : "hide"}>
-          <div style={{ fontWeight: "bold " }}>Question Asked</div>
-          <div className="closeButton">
-            <Button onClick={(e) => this.props.containerRendering("", "")}>
-              Close
-            </Button>
-          </div>
-          <div className="queAnsHeader">
-            <div>
-              <h5>{this.state.chatTime}</h5>
+        <div className="queAnsDialog">
+          <Divider />
+          <div className={this.state.question ? "showNew margin-20px" : "hide"}>
+            <div style={{ fontWeight: "bold " }}>Question Asked</div>
+            <div className="closeButton">
+              <Button onClick={(e) => this.props.containerRendering("", "")}>
+                Close
+              </Button>
             </div>
-            <div className="ratingRight">
-              <IconButton
-                onClick={(e) => this.setState({ reply: !this.state.reply })}
-              >
-                <UndoIcon />
-              </IconButton>
-              <IconButton onClick={this.likeQuestion}>
-                {this.state.like ? (
-                  <ThumbUpAltIcon color="primary" />
-                ) : (
-                  <ThumbUpAltIcon />
-                )}
-              </IconButton>
-              <IconButton onClick={this.likeQuestion}>
-                {this.state.like ? (
-                  <ThumbDownAltIcon />
-                ) : (
-                  <ThumbDownAltIcon color="primary" />
-                )}
-              </IconButton>
-            </div>
-          </div>
-          <div className="questionList">
-            <Typography variant="h5">{this.state.questionMsg}</Typography>
-          </div>
-        </div>
-        {this.state.replyList.map((reply) => {
-          return (
-            <div
-              className={this.state.question ? "showNew margin-20px" : "hide"}
-            >
-              <Divider />
-              <div className="replyHeader">
-                <div>
-                  <h5>
-                    {new Intl.DateTimeFormat("en-US", {
-                      year: "numeric",
-                      month: "2-digit",
-                      day: "2-digit",
-                      hour: "2-digit",
-                      minute: "2-digit",
-                      second: "2-digit",
-                    }).format(new Date(reply.createdDate))}
-                  </h5>
-                </div>
-                <div className="ratingRight">
-                  {/* <IconButton onClick={e => this.setState({reply:!this.state.reply})}>
-                    <UndoIcon />
-                  </IconButton> */}
-                  <IconButton
-                    onClick={(e) =>
-                      this.likeReply(
-                        reply.like.length ? !reply.like[0].like : true,
-                        reply.id
-                      )
-                    }
-                  >
-                    {reply.like.length ? (
-                      reply.like[0].like ? (
-                        <ThumbUpAltIcon color="primary" />
-                      ) : (
-                        <ThumbUpAltIcon />
-                      )
+            <div className="queAnsHeader">
+              <div>
+                <h5>{this.state.chatTime}</h5>
+              </div>
+              <div className="ratingRight">
+                <IconButton
+                  onClick={(e) => this.setState({ reply: !this.state.reply })}
+                >
+                  <UndoIcon />
+                </IconButton>
+                <Tooltip title="Like">
+                  <IconButton onClick={this.likeQuestion}>
+                    {this.state.like ? (
+                      <ThumbUpAltIcon color="primary" />
                     ) : (
                       <ThumbUpAltIcon />
                     )}
                   </IconButton>
-                  <IconButton
-                    onClick={(e) =>
-                      this.likeReply(
-                        reply.like.length ? !reply.like[0].like : true,
-                        reply.id
-                      )
-                    }
-                  >
-                    {reply.like.length ? (
-                      reply.like[0].like ? (
-                        <ThumbDownAltIcon />
-                      ) : (
-                        <ThumbDownAltIcon color="primary" />
-                      )
-                    ) : (
+                </Tooltip>
+                <Tooltip title="Unlike">
+                  <IconButton onClick={this.likeQuestion}>
+                    {this.state.like ? (
                       <ThumbDownAltIcon />
+                    ) : (
+                      <ThumbDownAltIcon color="primary" />
                     )}
                   </IconButton>
-                </div>
-              </div>
-              <div className="replyList">
-                <Typography variant="h5">{reply.message}</Typography>
+                </Tooltip>
               </div>
             </div>
-          );
-        })}
-        <Divider />
-        <div className={this.state.reply ? "showNew margin-20px" : "hide"}>
-          <Editor
-            editorState={this.state.editorState}
-            placeholder="enter your reply"
-            toolbarClassName="toolbarClassName"
-            wrapperClassName="wrapperClassName"
-            editorClassName="editorClassName"
-            onEditorStateChange={this.onEditorStateChangeRep}
-          />
-          <div id="askBtn">
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={(e) => this.replyOfAnswer()}
-            >
-              Reply
-            </Button>
+            <div className="questionList">
+              <Typography variant="h5">{this.state.questionMsg}</Typography>
+            </div>
+          </div>
+          {this.state.replyList.map((reply) => {
+            return (
+              <div
+                className={this.state.question ? "showNew margin-20px" : "hide"}
+              >
+                <Divider />
+                <div className="replyHeader">
+                  <div>
+                    <h5>
+                      {new Intl.DateTimeFormat("en-US", {
+                        year: "numeric",
+                        month: "2-digit",
+                        day: "2-digit",
+                        hour: "2-digit",
+                        minute: "2-digit",
+                        second: "2-digit",
+                      }).format(new Date(reply.createdDate))}
+                    </h5>
+                  </div>
+                  <div className="ratingRight">
+                    {/* <IconButton onClick={e => this.setState({reply:!this.state.reply})}>
+                    <UndoIcon />
+                  </IconButton> */}
+                    <Tooltip title="Like">
+                      <IconButton
+                        onClick={(e) =>
+                          this.likeReply(
+                            reply.like.length ? !reply.like[0].like : true,
+                            reply.id
+                          )
+                        }
+                      >
+                        {reply.like.length ? (
+                          reply.like[0].like ? (
+                            <ThumbUpAltIcon color="primary" />
+                          ) : (
+                            <ThumbUpAltIcon />
+                          )
+                        ) : (
+                          <ThumbUpAltIcon />
+                        )}
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Unlike">
+                      <IconButton
+                        onClick={(e) =>
+                          this.likeReply(
+                            reply.like.length ? !reply.like[0].like : true,
+                            reply.id
+                          )
+                        }
+                      >
+                        {reply.like.length ? (
+                          reply.like[0].like ? (
+                            <ThumbDownAltIcon />
+                          ) : (
+                            <ThumbDownAltIcon color="primary" />
+                          )
+                        ) : (
+                          <ThumbDownAltIcon />
+                        )}
+                      </IconButton>
+                    </Tooltip>
+                  </div>
+                </div>
+                <div className="replyList">
+                  <Typography variant="h5">{reply.message}</Typography>
+                </div>
+              </div>
+            );
+          })}
+          <Divider />
+          <div className={this.state.reply ? "showNew margin-20px" : "hide"}>
+            <Editor
+              editorState={this.state.editorState}
+              placeholder="enter your reply"
+              toolbarClassName="toolbarClassName"
+              wrapperClassName="wrapperClassName"
+              editorClassName="editorClassName"
+              onEditorStateChange={this.onEditorStateChangeRep}
+            />
+            <div id="askBtn">
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={(e) => this.replyOfAnswer()}
+              >
+                Reply
+              </Button>
+            </div>
           </div>
         </div>
       </div>
