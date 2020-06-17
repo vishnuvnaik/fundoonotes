05e50ef9ac2d1a7199a4854a4147d4196d0266d9
= <=>
2020-06-18 00:36:58 +0530

    [Vishnu]Refactor.removed async await from registration function

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 import userApiConstants from "../constants/userApiConstants";
 
-async function userRegistration(registerData) {
+function userRegistration(registerData) {
   try {
-    const response = await axios.post(
+    const response = axios.post(
       process.env.REACT_APP_BASE_URL + userApiConstants.registration,
       registerData
     );
     return response;
   } catch (err) {
-    throw err;
+    return err;
   }
 }
 
